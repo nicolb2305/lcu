f9162258633cb071f5ebfaea675d0bc3648595fd
nicolb2305 <nico@dsl.no>
2023-10-02 10:00:43 +0200

    Fixed standalone programs to work with async

--- custom-game-creator/src/main.rs
+++ custom-game-creator/src/main.rs
@@ -1,7 +1,8 @@
 use client_api::{actions::create_custom, client::Client};
 use eyre::Result;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let client = Client::new()?;
-    Ok(create_custom(&client)?)
+    Ok(create_custom(&client).await?)
 }
--- custom-game-randomizer/src/main.rs
+++ custom-game-randomizer/src/main.rs
@@ -1,7 +1,8 @@
 use client_api::{actions::randomize_teams, client::Client};
 use eyre::Result;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let client = Client::new()?;
-    Ok(randomize_teams(&client)?)
+    Ok(randomize_teams(&client).await?)
 }
